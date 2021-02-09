e1439f4502cc9096c57c96525133dd6a940589c2
Messner Daniel (Student Com19) <daniel.messner@stud-inf.unibz.it>
2021-02-09 11:21:51 +0100

    Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+ <v-app>
+
+   <v-navigation-drawer app
+      v-model="drawer"
+      absolute
+      temporary
+      right
+   >
+        <!-- Menu Links -->
+        <v-list>
+          <v-list-item to="/" exact>
+            <v-list-item-action>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Home</v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/about">
+            <v-list-item-action>
+              <v-icon>mdi-information</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>About</v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+  </v-navigation-drawer>
+
+  <v-app-bar clipped-right app>
+     <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+    name: "App",
+     data: () => ({
+      drawer: false,
+    }),
+  };
+</script>
